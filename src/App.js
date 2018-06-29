2fcf19dd03a4795b6db91d197befde22420da175
sivakorn <sivakorn@9picompany.com>
2018-06-29 16:51:12 +0700

    fix css

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
-				<h1 style={{fontSize: '40px'}}>Hello</h1>
+				<h1 style={{fontSize: '50px'}}>Hello</h1>
 				<TodoInput addTodo={this.addTodo}/>
 				<TodoList items={items}/>
 			</div>
